89856425fe664f9693ee675e291ba82caeee32d4
zhijiezh <jerry_shh@hotmail.com>
2022-04-08 15:33:20 -0400

    feat(*): Make grappling working on both hands

--- Assets/Grappling.cs
+++ Assets/Grappling.cs
@@ -7,14 +7,24 @@ using Microsoft.MixedReality.Toolkit.Input;
 public class Grappling : MonoBehaviour
 {
 
-    private LineRenderer lr;
-    private Vector3 grapplePoint;
+    public LineRenderer left_lr;
+    public LineRenderer right_lr;
+
     public LayerMask whatIsGrappleable;
-    public Transform gunTip, camera, player;
+    public Transform camera, player;
     private float maxDistance = 100f;
-    private SpringJoint joint;
+    private SpringJoint left_joint;
+    private SpringJoint right_joint;
 
     private IMixedRealityInputSystem inputSystem = null;
+    private IMixedRealityController leftController;
+    private IMixedRealityController rightController;
+
+
+    private Vector3 leftGrapplePoint;
+    private Vector3 rightGrapplePoint;
+    private Vector3 leftCurrentGrapplePosition;
+    private Vector3 rightCurrentGrapplePosition;
 
     protected IMixedRealityInputSystem InputSystem
     {
@@ -27,21 +37,38 @@ public class Grappling : MonoBehaviour
             return inputSystem;
         }
     }
-
-    void Awake()
-    {
-        lr = GetComponent<LineRenderer>();
-    }
-
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+         foreach (IMixedRealityController controller in InputSystem.DetectedControllers)
         {
-            StartGrapple();
+            if (controller.Visualizer?.GameObjectProxy != null)
+            {
+                if (controller.Visualizer?.GameObjectProxy.name == "Left_OVRControllerPrefab")
+                {
+                    leftController = controller;
+                }
+                else if (controller.Visualizer?.GameObjectProxy.name == "Right_OVRControllerPrefab")
+                {
+                    rightController = controller;
+                }
+            }
         }
-        else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
-            StopGrapple();
+            StartGrapple(leftController, left_lr,true);
+        }
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            StartGrapple(rightController, right_lr,false);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            StopGrapple(left_joint, left_lr);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            StopGrapple(right_joint, right_lr);
         }
     }
 
@@ -54,50 +81,72 @@ public class Grappling : MonoBehaviour
     /// <summary>
     /// Call whenever we want to start a grapple
     /// </summary>
-    void StartGrapple()
+    void StartGrapple(IMixedRealityController controller, LineRenderer lr, bool isLeft)
     {
         RaycastHit hit;
 
-        foreach (IMixedRealityController controller in InputSystem.DetectedControllers)
+
+        //Debug.Log("Visualizer Game Object: " + controller.Visualizer.GameObjectProxy);
+        Vector3 forward = controller.Visualizer.GameObjectProxy.GetComponent<Transform>().forward;
+        Vector3 pos = controller.Visualizer.GameObjectProxy.GetComponent<Transform>().position;
+        if (Physics.Raycast(pos, forward, out hit, maxDistance, whatIsGrappleable))
         {
-            if (controller.Visualizer?.GameObjectProxy != null)
+            
+            if (isLeft)
             {
-                //Debug.Log("Visualizer Game Object: " + controller.Visualizer.GameObjectProxy);
-                Vector3 forward = controller.Visualizer.GameObjectProxy.GetComponent<Transform>().forward;
-                Vector3 pos = controller.Visualizer.GameObjectProxy.GetComponent<Transform>().position;
-                if (Physics.Raycast(pos, forward, out hit , maxDistance, whatIsGrappleable))
+                leftGrapplePoint = hit.point;
+                if (left_joint != null)
                 {
-                    grapplePoint = hit.point;
-                    if (joint != null)
-                    {
-                        Destroy(joint);
-                    }
-                    joint = player.gameObject.AddComponent<SpringJoint>();
-                    joint.autoConfigureConnectedAnchor = false;
-                    joint.connectedAnchor = grapplePoint;
-
-                    float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
-
-                    //The distance grapple will try to keep from grapple point. 
-                    joint.maxDistance = distanceFromPoint * 0.8f;
-                    joint.minDistance = distanceFromPoint * 0.25f;
-
-                    //Adjust these values to fit your game.
-                    joint.spring = 4.5f;
-                    joint.damper = 7f;
-                    joint.massScale = 4.5f;
-
-                    lr.positionCount = 2;
-                    currentGrapplePosition = gunTip.position;
+                    Destroy(left_joint);
                 }
-                Debug.Log("Visualizer Game Object: " + forward.x + forward.y + forward.z);
+                left_joint = player.gameObject.AddComponent<SpringJoint>();
+                left_joint.autoConfigureConnectedAnchor = false;
+                left_joint.connectedAnchor = leftGrapplePoint;
+
+                float distanceFromPoint = Vector3.Distance(player.position, leftGrapplePoint);
+
+                //The distance grapple will try to keep from grapple point. 
+                left_joint.maxDistance = distanceFromPoint * 0.8f;
+                left_joint.minDistance = distanceFromPoint * 0.25f;
+
+                //Adjust these values to fit your game.
+                left_joint.spring = 4.5f;
+                left_joint.damper = 7f;
+                left_joint.massScale = 4.5f;
             }
             else
             {
-                Debug.Log("Controller has no visualizer!");
+                rightGrapplePoint = hit.point;
+                if (right_joint != null)
+                {
+                    Destroy(right_joint);
+                }
+                right_joint = player.gameObject.AddComponent<SpringJoint>();
+                right_joint.autoConfigureConnectedAnchor = false;
+                right_joint.connectedAnchor = rightGrapplePoint;
+
+                float distanceFromPoint = Vector3.Distance(player.position, rightGrapplePoint);
+
+                //The distance grapple will try to keep from grapple point. 
+                right_joint.maxDistance = distanceFromPoint * 0.8f;
+                right_joint.minDistance = distanceFromPoint * 0.25f;
+
+                //Adjust these values to fit your game.
+                right_joint.spring = 4.5f;
+                right_joint.damper = 7f;
+                right_joint.massScale = 4.5f;
             }
+            
 
-
+            lr.positionCount = 2;
+            if (isLeft)
+            {
+                leftCurrentGrapplePosition = pos;
+            }
+            else
+            {
+                rightCurrentGrapplePosition = pos;
+            }
         }
     }
 
@@ -105,32 +154,38 @@ public class Grappling : MonoBehaviour
     /// <summary>
     /// Call whenever we want to stop a grapple
     /// </summary>
-    void StopGrapple()
+    void StopGrapple(SpringJoint joint, LineRenderer lr)
     {
         lr.positionCount = 0;
         Destroy(joint);
     }
 
-    private Vector3 currentGrapplePosition;
+
 
     void DrawRope()
     {
         //If not grappling, don't draw rope
-        if (!joint) return;
-
-        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
-
-        lr.SetPosition(0, gunTip.position);
-        lr.SetPosition(1, currentGrapplePosition);
+        if (left_joint)
+        {
+            leftCurrentGrapplePosition = Vector3.Lerp(leftCurrentGrapplePosition, leftGrapplePoint, Time.deltaTime * 8f);
+            left_lr.SetPosition(0, leftController.Visualizer.GameObjectProxy.GetComponent<Transform>().position);
+            left_lr.SetPosition(1, leftCurrentGrapplePosition);
+        }
+        if (right_joint)
+        {
+            rightCurrentGrapplePosition = Vector3.Lerp(rightCurrentGrapplePosition, rightGrapplePoint, Time.deltaTime * 8f);
+            right_lr.SetPosition(0, rightController.Visualizer.GameObjectProxy.GetComponent<Transform>().position);
+            right_lr.SetPosition(1, rightCurrentGrapplePosition);
+        }
     }
 
     public bool IsGrappling()
     {
-        return joint != null;
+        return left_joint != null || right_joint != null;
     }
 
-    public Vector3 GetGrapplePoint()
+    public Vector3[] GetGrapplePoints()
     {
-        return grapplePoint;
+        return new Vector3[] { leftGrapplePoint, rightGrapplePoint };
     }
 }
